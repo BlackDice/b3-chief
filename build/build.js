071f6419f259d3c96cf9f199f94bc1836e857b50
Daniel K <fredy.c@seznam.cz>
2016-06-04 15:01:03 +0200

    Tweaks of build process + minified full bundle
    Getting rid of transform-runtime in favor of requiring
    babel-polyfill instead which will be installed as dependency.
    
    chief.browser.js ... transformed to ES5 environement without deps
    chief.full.js ... ES5 + deps

--- build/build.js
+++ build/build.js
@@ -9,8 +9,17 @@ import rollup from 'rollup';
 import babel from 'rollup-plugin-babel';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonJs from 'rollup-plugin-commonjs';
+import uglify from 'rollup-plugin-uglify';
 
 const moduleName = 'chief';
+
+const babelConfig = {
+	babelrc: false,
+	exclude: 'node_modules/**',
+	presets: ['stage-1'],
+	plugins: ['dev-expression'],
+	runtimeHelpers: true,
+};
 
 async function execute() {
 	await fs.ensureDir('dist').catch(() => true);
@@ -18,49 +27,65 @@ async function execute() {
 
 	await Promise.all([
 		makeBundle(
-			{ format: 'cjs', ext: '.js' },
+			{ format: 'es6', ext: '.mjs', package: 'jsnext:main',
+				babelPlugins: ['external-helpers'],
+			}
 		),
 		makeBundle(
-			{ format: 'es6', ext: '.mjs' }
+			{ format: 'cjs', ext: '.js', package: 'main',
+				babelPlugins: ['external-helpers'],
+			},
 		),
 		makeBundle(
-			{ format: 'cjs', ext: '.browser.js',
+			{ format: 'cjs', ext: '.browser.js', package: 'browser',
 				babelPresets: ['es2015-rollup'],
-				babelPlugins: ['transform-runtime'],
 			}
 		),
 		makeBundle(
 			{ format: 'umd', ext: '.full.js', moduleId: 'b3chief',
 				babelPresets: ['es2015-rollup'],
-				babelPlugins: ['transform-runtime', 'lodash'],
+				babelPlugins: ['lodash'],
 			}
 		),
-		writePackage(),
+		makeBundle(
+			{ format: 'umd', ext: '.full.min.js', moduleId: 'b3chief', minify: true,
+				babelPresets: ['es2015-rollup'],
+				babelPlugins: ['lodash'],
+			}
+		),
 		fs.copy('LICENSE.txt', 'dist/LICENSE.txt'),
 		fs.copy('README.md', 'dist/README.md'),
-	]);
+	]).then(writePackage);
 }
 
 async function makeBundle(config) {
 	const isUMD = config.format === 'umd';
+
+	const localBabelConfig = {
+		...babelConfig,
+		presets: babelConfig.presets.concat(config.babelPresets || []),
+		plugins: babelConfig.plugins.concat(config.babelPlugins || []),
+	};
+
 	const inputConfig = {
 		entry: 'src/index.js',
 		plugins: [
-			babel({
-				babelrc: false,
-				exclude: 'node_modules/**',
-				presets: ['stage-1'].concat(config.babelPresets || []),
-				plugins: config.babelPlugins || [],
+			babel(localBabelConfig),
+			nodeResolve({
+				jsnext: true, main: true, browser: isUMD, preferBuiltins: !isUMD,
 			}),
-			nodeResolve({ preferBuiltins: true, browser: isUMD }),
-			commonJs(),
+			commonJs({ ignoreGlobal: true }),
 		],
 	};
 
 	if (isUMD) {
-
+		inputConfig.external = ['babel-polyfill'];
 	} else {
 		inputConfig.external = Object.keys(pkg.dependencies);
+	}
+
+	if (config.minify) {
+		inputConfig.plugins.push(uglify());
 	}
 
 	const outputConfig = {
@@ -68,21 +93,35 @@ async function makeBundle(config) {
 		format: config.format,
 		sourceMap: !config.minify,
 		moduleId: config.moduleId,
-		moduleName,
+		moduleName: config.moduleId || moduleName,
+		globals: {
+			'babel-polyfill': '_babelPolyfill',
+		},
 	};
 
 	const bundle = await rollup.rollup(inputConfig);
 	await bundle.write(outputConfig);
 	console.log('created', outputConfig.dest);
+
+	if (config.package) {
+		return { [config.package]: `${moduleName}${config.ext}` };
+	}
+
+	return null;
 }
 
-function writePackage() {
+function writePackage(mainfiles) {
 	Reflect.deleteProperty(pkg, 'private');
 	Reflect.deleteProperty(pkg, 'devDependencies');
 	Reflect.deleteProperty(pkg, 'scripts');
 	Reflect.deleteProperty(pkg, 'babel');
 	Reflect.deleteProperty(pkg, 'ava');
 	Reflect.deleteProperty(pkg, 'nyc');
+	for (const mainfile of mainfiles) {
+		if (mainfile !== null) {
+			Object.assign(pkg, mainfile);
+		}
+	}
 	return fs.writeFile('dist/package.json', JSON.stringify(pkg, null, '  '), 'utf-8');
 }
 
